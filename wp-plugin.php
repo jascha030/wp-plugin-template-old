b4901318261bd301be7561767ee400b3ee2b4f13
Jascha van Aalst <mr.jascha@gmail.com>
2021-03-20 19:22:34 +0100

    Started main plugin file

---
+++ wp-plugin.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * VENDOR_NAME/PACKAGE_NAME
+ *
+ * Plugin Name: PLUGIN_NAME
+ * Plugin Description: PLUGIN_DESC
+ * Author: PLUGIN_AUTHOR
+ * Author Uri: AUTHOR_URI
+ */
+
+/**
+ * Init main plugin class
+ */
+add_action('plugins_loaded', static function () {});
