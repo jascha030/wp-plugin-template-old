86f86e05f0302036c41e90a122ee2ea832e5ef8b
Jascha van Aalst <mr.jascha@gmail.com>
2021-03-21 22:33:58 +0100

    plugin header file

---
+++ includes/plugin-header.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * @wordpress-plugin
+ * VENDOR_NAME/PACKAGE_NAME
+ *
+ * Plugin Name: PLUGIN_NAME
+ * Plugin Description: PLUGIN_DESC
+ * Author: PLUGIN_AUTHOR
+ * Author Uri: AUTHOR_URI
+ */
--- wp-plugin.php
+++ wp-plugin.php
@@ -1,14 +1,34 @@
 <?php
-/**
- * VENDOR_NAME/PACKAGE_NAME
- *
- * Plugin Name: PLUGIN_NAME
- * Plugin Description: PLUGIN_DESC
- * Author: PLUGIN_AUTHOR
- * Author Uri: AUTHOR_URI
- */
+
+namespace Jascha030;
+
+use Jascha030\PluginLib\Plugin\ConfigurablePluginApiRegistry;
+
+loadFile(__DIR__ . '/includes/plugin-header.php');
+loadFile(__DIR__ . '/vendor/autoload.php', ', please try running `composer install` in the console');
+
+$plugin = new ConfigurablePluginApiRegistry('PLUGIN_NAME', __DIR__ . '/config/config.php');
 
 /**
  * Init main plugin class
  */
-add_action('plugins_loaded', static function () {});
+add_action(
+    'plugins_loaded',
+    static function () {
+    }
+);
+
+/**
+ * Includes a file, throws an exception when not found.
+ *
+ * @param string $fileName
+ * @param string $errorText
+ */
+function loadFile(string $fileName, string $errorText = ''): void
+{
+    if (!is_file(__DIR__ . $fileName)) {
+        throw new \RuntimeException("Couldn\'t find file: '{$fileName}'", $errorText);
+    }
+
+    include $fileName;
+}
